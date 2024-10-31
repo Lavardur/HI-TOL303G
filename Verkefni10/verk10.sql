6b2e8b51ac89c38bf1ac71dfb5d925f0b35c1eca
Lavardur <antonben44@gmail.com>
2024-10-31 21:28:46 +0000

    helling gleymdi að committa

---
+++ Verkefni10/verk10.sql
@@ -0,0 +1,4 @@
+-- Find the last names of all middle managers, i.e. those employees who supervise
+-- someone and are supervised by someone. You may assume that no employee
+-- supervises himself.
+SELECT last_name FROM EMPLOYEE
